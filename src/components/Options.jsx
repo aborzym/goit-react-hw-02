e14822d1df34a3425bbf8ee7af55567b1e43d1b7
Andrzej Borzym <borzym.a@gmail.com>
2024-11-06 16:56:22 +0100

    options styled

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,12 +2,22 @@ import css from "./Options.module.css";
 
 const Options = ({ updateFeedback, reset, showResetButton }) => {
   return (
-    <>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>{" "}
-      {showResetButton && <button onClick={reset}>Reset</button>}
-    </>
+    <div className={css.container}>
+      <button className={css.good} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+      <button className={css.neutral} onClick={() => updateFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className={css.bad} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+      {showResetButton && (
+        <button className={css.reset} onClick={reset}>
+          Reset
+        </button>
+      )}
+    </div>
   );
 };
 
